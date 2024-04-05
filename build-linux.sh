39891cc90f92f638343709059bd959f63362be46
Hownz <994703865@qq.com>
2024-04-05 12:18:18 +0800

    2024-04-05 12:18:00 第一次提交

--- build-linux.sh
+++ build-linux.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-set -e
+set -e # 脚本执行过程中遇到错误时立即退出
 
 echo "$0 $@"
 while getopts ":t:a:d:b:m" opt; do
+  # -t : target (rk356x/rk3588/rk3576/rv1106/rk1808/rv1126)
+  # -a : arch (aarch64/armhf)
+  # -d : demo name
+  # -b : build_type (Debug/Release)
+  # -m : enable address sanitizer, build_type need set to Debug
   case $opt in
-    t)
+    t) # -t
       TARGET_SOC=$OPTARG
       ;;
-    a)
+    a) # -a
       TARGET_ARCH=$OPTARG
       ;;
-    b)
+    b) # -b
       BUILD_TYPE=$OPTARG
       ;;
-    m)
+    m) # -m
+      # ASAN是AddressSanitizer的缩写，是一种内存错误检测工具，可以检测出内存访问越界、内存泄漏等问题
       ENABLE_ASAN=ON
       export ENABLE_ASAN=TRUE
       ;;
-    d)
+    d) # -d
       BUILD_DEMO_NAME=$OPTARG
       ;;
     :)
@@ -31,7 +37,7 @@ while getopts ":t:a:d:b:m" opt; do
   esac
 done
 
-if [ -z ${TARGET_SOC} ] || [ -z ${BUILD_DEMO_NAME} ]; then
+if [ -z ${TARGET_SOC} ] || [ -z ${BUILD_DEMO_NAME} ]; then # -z检查
   echo "$0 -t <target> -a <arch> -d <build_demo_name> [-b <build_type>] [-m]"
   echo ""
   echo "    -t : target (rk356x/rk3588/rk3576/rv1106/rk1808/rv1126)"
@@ -45,7 +51,8 @@ if [ -z ${TARGET_SOC} ] || [ -z ${BUILD_DEMO_NAME} ]; then
   exit -1
 fi
 
-if [[ -z ${GCC_COMPILER} ]];then
+# 配置编译器宏 GCC_COMPILER ，rk3588 GCC_COMPILER=aarch64-linux-gnu
+if [[ -z ${GCC_COMPILER} ]];then # -z是判断字符串长度是否为0
     if [[ ${TARGET_SOC} = "rv1106"  || ${TARGET_SOC} = "rv1103" ]];then
         echo "Please set GCC_COMPILER for $TARGET_SOC"
         echo "such as export GCC_COMPILER=~/opt/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf"
@@ -53,13 +60,16 @@ if [[ -z ${GCC_COMPILER} ]];then
     elif [[ ${TARGET_SOC} = "rv1109" || ${TARGET_SOC} = "rv1126" ]];then
         GCC_COMPILER=arm-linux-gnueabihf
     else
-        GCC_COMPILER=aarch64-linux-gnu
+        GCC_COMPILER=aarch64-linux-gnu # aarch64-linux-gnu-gcc
     fi
 fi
+# 导入环境变量
 echo "$GCC_COMPILER"
 export CC=${GCC_COMPILER}-gcc
 export CXX=${GCC_COMPILER}-g++
 
+# 检查 ${CC} 变量所代表的命令是否可用。如果命令可用，继续执行后续操作；
+# 如果命令不可用，打印错误提示信息并退出脚本。
 if command -v ${CC} >/dev/null 2>&1; then
     :
 else
@@ -74,11 +84,14 @@ if [[ -z ${BUILD_TYPE} ]];then
     BUILD_TYPE=Release
 fi
 
+# 使用地址清理程序构建为了进行内存检查，BUILD_TYPE需要设置为Debug
 # Build with Address Sanitizer for memory check, BUILD_TYPE need set to Debug
 if [[ -z ${ENABLE_ASAN} ]];then
     ENABLE_ASAN=OFF
 fi
 
+# 这段代码的作用是在 examples 目录及其子目录中查找名称为 ${BUILD_DEMO_NAME} 的文件或目录。
+# 然后，检查找到的路径中是否存在 cpp 子目录，如果存在，将该路径赋值给 BUILD_DEMO_PATH 变量
 for demo_path in `find examples -name ${BUILD_DEMO_NAME}`
 do
     if [ -d "$demo_path/cpp" ]
@@ -96,8 +109,8 @@ then
     do
         if [ -d "$demo_path" ]
         then
-            dname=`dirname "$demo_path"`
-            name=`basename $dname`
+            dname=`dirname "$demo_path"` # dirname 是一个命令，用于获取指定路径的父目录路径
+            name=`basename $dname` # basename 是一个命令，用于获取指定路径的基本文件名
             echo "$name"
         fi
     done
@@ -148,7 +161,9 @@ TARGET_PLATFORM=${TARGET_SOC}_linux
 if [[ -n ${TARGET_ARCH} ]];then
 TARGET_PLATFORM=${TARGET_PLATFORM}_${TARGET_ARCH}
 fi
-ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
+# ROOT_PWD 
+ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd ) 
+# $SOURCE 当前脚本的绝对路径
 INSTALL_DIR=${ROOT_PWD}/install/${TARGET_PLATFORM}/${TARGET_SDK}
 BUILD_DIR=${ROOT_PWD}/build/build_${TARGET_SDK}_${TARGET_PLATFORM}_${BUILD_TYPE}
 
@@ -187,7 +202,7 @@ make install
 # Check if there is a rknn model in the install directory
 suffix=".rknn"
 shopt -s nullglob
-if [ -d "$INSTALL_DIR" ]; then
+if [ -d "$INSTALL_DIR" ]; then # -d 检查文件是否存在
     files=("$INSTALL_DIR/model/"/*"$suffix")
     shopt -u nullglob
 
